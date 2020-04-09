ce6a54e9886f96152a343eb1f84281742ab79f16
vorobei_yaroslav <yaroslav.vorobei@playtika.com>
2020-04-09 20:05:36 +0300

    add simple endpoint structure

---
+++ src/main/java/services/walletService/controllers/WalletController.java
@@ -0,0 +1,50 @@
+package services.walletService.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.walletService.BaseResponse;
+import services.walletService.PaymentRequest;
+
+
+@RestController
+@RequestMapping("/wallet-servise")
+public class WalletController {
+    private final String sharedKey = "SHARED_KEY";
+
+    private static final String SUCCESS_STATUS = "success";
+    private static final String ERROR_STATUS = "error";
+    private static final int CODE_SUCCESS = 200;
+    private static final int AUTH_FAILURE = 400;
+
+    @GetMapping
+    public BaseResponse showStatus() {
+        return new BaseResponse(SUCCESS_STATUS, 201);
+    }
+
+
+    @GetMapping("/badgateway")
+    public ResponseEntity testErrorStatus() {
+
+        return new ResponseEntity(HttpStatus.BAD_GATEWAY);
+    }
+
+    @PostMapping("/pay")
+    public BaseResponse pay(@RequestParam(value = "key") String key, @RequestBody PaymentRequest request) {
+
+        final BaseResponse response;
+
+        if (sharedKey.equalsIgnoreCase(key)) {
+            int userId = request.getUserId();
+            String itemId = request.getItemId();
+            double discount = request.getDiscount();
+            // Process the request
+            // ....
+            // Return success response to the client.
+            response = new BaseResponse(SUCCESS_STATUS, CODE_SUCCESS);
+        } else {
+            response = new BaseResponse(ERROR_STATUS, AUTH_FAILURE);
+        }
+        return response;
+    }
+}
