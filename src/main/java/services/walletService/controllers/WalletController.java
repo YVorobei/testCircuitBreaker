ebc28d96c614648e9833a5e43e34678a5cc356d1
vorobei_yaroslav <yaroslav.vorobei@playtika.com>
2020-04-14 14:53:24 +0300

    refactoring

--- src/main/java/services/walletService/controllers/WalletController.java
+++ src/main/java/services/walletService/controllers/WalletController.java
@@ -18,16 +18,23 @@ import static org.springframework.http.HttpStatus.*;
 @RequestMapping("/wallet")
 public class WalletController {
 
-    private Logger logger = LoggerFactory.getLogger(WalletController.class);
     private HttpStatus httpStatusResponse = OK;
+    private RestTemplate restTemplate = new RestTemplate();
+    private List<HttpMessageConverter<?>> converters = new ArrayList<>();
+    private Logger logger = LoggerFactory.getLogger(WalletController.class);
+    private StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+    private String updateWalletUrl = "http://wallet-service-01-test.dublin.local:3332/wallet/updateWallet?appToken={appToken}";
     private int responseTimeout;
+    private int counter;
 
     @PostMapping("/setResponseCodeWithTimeout")
     public String setResponseCodeWithTimeout(@RequestParam(value = "responseCode") int responseCode, @RequestParam(value = "timeout") int timeout){
         httpStatusResponse = new HttpStatusMapping().getHttpStatus(responseCode);
         responseTimeout = timeout;
-        String infoLog = "Set httpStatusResponse: " + httpStatusResponse + "; with timeout: " + timeout;
-        return infoLog;
+        counter = 0;
+        String logInfo = "Set httpStatusResponse: " + httpStatusResponse + "; with timeout: " + timeout;
+        logger.info(logInfo);
+        return logInfo;
     }
 
     @PostMapping("/updateWallet")
@@ -37,26 +44,28 @@ public class WalletController {
         return updateWalletResponse(appToken, requestHeaders, requestBody);
     }
 
-    public ResponseEntity<String> updateWalletResponse(String appToken, HttpHeaders requestHeaders, String requestBody) throws InterruptedException {
-        String updateWalletUrl = "http://wallet-service-01-test.dublin.local:3332/wallet/updateWallet?appToken={appToken}";
-        if(!httpStatusResponse.is2xxSuccessful()){
+    private ResponseEntity<String> updateWalletResponse(String appToken, HttpHeaders requestHeaders, String requestBody) throws InterruptedException {
+         if(!httpStatusResponse.is2xxSuccessful()){
+            counter++;
+            logger.info("Counter for error request: "+ counter);
             return new ResponseEntity(httpStatusResponse);
         }
         HttpEntity<String> entity = new HttpEntity<String>(requestBody, requestHeaders);
-        RestTemplate restTemplate = new RestTemplate();
-        List<HttpMessageConverter<?>> converters = new ArrayList<>();
-        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
-
-        stringConverter.setWriteAcceptCharset(false);
-        converters.add(stringConverter);
-        restTemplate.setMessageConverters(converters);
-
+        setMessageConverters(restTemplate);
         ResponseEntity<String> response = restTemplate.exchange(updateWalletUrl, HttpMethod.POST, entity, String.class, appToken);
+
         logger.info(updateWalletUrl);
         logger.info(response.getStatusCode().toString());
         logger.info("Request body: " + requestBody);
         logger.info("Response body: "+ response.getBody());
         Thread.sleep(responseTimeout);
+
         return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+    }
+
+    private void setMessageConverters(RestTemplate restTemplate){
+        stringConverter.setWriteAcceptCharset(false);
+        converters.add(stringConverter);
+        restTemplate.setMessageConverters(converters);
     }
 }
